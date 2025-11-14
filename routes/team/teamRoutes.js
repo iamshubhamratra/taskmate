85892be39ad17476f764ae7239c5c6b81ec5e825
iamshubhamratra <ratrashubham129@gmail.com>
2025-11-15 00:21:07 +0530

    update team name api done

--- routes/team/teamRoutes.js
+++ routes/team/teamRoutes.js
@@ -4,11 +4,16 @@ const createTeam = require("../../controllers/teamController/createTeam");
 const createTeamValidator = require("../../validator/team/createTeamValidator");
 const deleteTeamValidator = require("../../validator/team/deleteTeamValidator");
 const deleteTeam = require("../../controllers/teamController/deleteTeam");
+const updateTeamNameValidator = require("../../validator/team/upadteTeamValidator");
+const updateTeamName = require("../../controllers/teamController/updateTeamName");
 
 // create team
 teamRouter.post("/createTeam", createTeamValidator, createTeam);
 
 // delete team
 teamRouter.post("/deleteTeam", deleteTeamValidator, deleteTeam);
+
+// update team name
+teamRouter.post("/updateTeam", updateTeamNameValidator, updateTeamName);
 
 module.exports = teamRouter;
